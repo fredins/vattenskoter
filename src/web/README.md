f5ea0442cb396ecdf74899c87a04fcf83b14a954
Spektria <48885787+Spektria@users.noreply.github.com>
2022-04-04 13:28:30 +0200

    added boiler plate project

---
+++ src/web/README.md
@@ -0,0 +1,3 @@
+# react-ts-tw-boilerplate
+Boilerplate for a project using React, Typescript, and Tailwind.
+Also include devDependencies in package.json for my neovim setup.
---
+++ src/web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-2xl text-center font-bold text-blue-900"> Hello world! </p>
+    </div>
+  );
+}
+
+export default App;
