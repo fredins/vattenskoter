867adfc19092e1547ecb4136d1f849facb915984
funcorange <love.njal@telia.com>
2022-04-05 16:31:10 +0200

    Add basic session view component

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SessionView from './components/sessionview';
 
 const App = () => {
   return (
     <div>
       <p className="text-2xl text-center font-bold text-blue-900"> Hello world! </p>
+	  <SessionView />
     </div>
   );
 }
---
+++ web/src/app/components/sessionview.tsx
@@ -0,0 +1,9 @@
+import react from 'React';
+
+const SessionView = () : JSX.Element => {
+	return (
+		<p>Uppkörningstillfälle</p>
+	);
+}
+
+export default SessionView;
