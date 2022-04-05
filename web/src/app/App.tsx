c044c685b22bd5a9e859c4a92646407a49eb1c31
fredins <fredin.martin@gmail.com>
2022-04-05 14:49:17 +0200

    Implement react-awesome-calendar

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import Calendar,{date, event} from 'react-awesome-calendar';
 
 const App = () => {
   return (
     <div>
-      <p className="text-2xl text-center font-bold text-blue-900"> Hello world! </p>
+    <div className='pl-10 pr-10'>
+      <Calendar
+        events = {events}
+        onClickEvent={(id : number) => alert('event id: ' + id) }
+        onClickTimeLine={(date : date) => alert('create an event at date: ' + JSON.stringify(date))}
+      />
+    </div>
     </div>
   );
 }
+
+const events : event[] = [{
+  id: 1,
+  color: '#fd3153',
+  from: new Date('2022-04-04T17:00:00+00:00'),
+  to: new Date('2022-04-04T18:00:00+00:00'),
+  title: 'This is an event'
+}];
 
 export default App;
