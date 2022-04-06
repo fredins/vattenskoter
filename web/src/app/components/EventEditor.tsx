1acb58ef7f8a463070af33294d2cfd18579b430e
fredins <fredin.martin@gmail.com>
2022-04-06 20:38:32 +0200

    Create a simple example component for input handling

--- web/src/app/components/EventEditor.tsx
+++ web/src/app/components/EventEditor.tsx
@@ -1,5 +1,15 @@
 import { useState } from 'react';
 
+/*
+Simple startkomponent for inputs. 
+
+useState : är en react hook https://reactjs.org/docs/hooks-state.html
+title?   : frågetecknet betyder att fielden inte är nödvändig (att den kan vara undefined) 
+=> : är symbolen för en lamdafunktion
+className : är var man stoppar in tailwind-funktioner/klasser https://tailwindcss.com/docs/utility-first
+*/
+
+
 type Data = { title?      : string
             , from?       : Date
             , to?         : Date
@@ -11,7 +21,6 @@ type Data = { title?      : string
 const EventEditor = (): JSX.Element => {
   const [state, setState] = useState<Data>()
 
-      /* {state} */
   return (
     <div className="grid place-items-center h-screen">
       <form onSubmit={() => console.log("Check the data and save in database if ok")}>
@@ -19,6 +28,7 @@ const EventEditor = (): JSX.Element => {
           Title:   
           <input type="text" value={state?.title} onChange={e => setState({title: e.target.value})}/>
         </label>
+        <p>{state?.title}</p>
       </form>
     </div>
   )
