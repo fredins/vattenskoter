9d6a9cf913b8566105f5535c9e59a2bf0dfb8d66
funcorange <love.njal@telia.com>
2022-04-06 15:55:37 +0200

    Add hardcoded session information view

--- web/src/app/components/sessionview.tsx
+++ web/src/app/components/sessionview.tsx
@@ -2,7 +2,13 @@ import react from 'React';
 
 const SessionView = () : JSX.Element => {
 	return (
-		<h2>Uppkörningstillfälle</h2>
+		<div>
+			<h2>Uppkörningstillfälle</h2>
+			<b>Plats:</b> Vid vattnet<br/>
+			<b>Tid:</b> 12:00 2022-04-06<br/>
+			<b>Instruktörer:</b> Bengt Bengtsson<br/>
+			<b>Deltagare:</b> Alice Albertsson, Carl Carlsson<br/>
+		</div>
 	);
 }
 
