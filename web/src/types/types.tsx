a69c3a6ab4d7a1968a08e269c1235186a6ff2383
fredins <fredin.martin@gmail.com>
2022-04-07 17:43:37 +0200

    Make a file for shared types

---
+++ web/src/types/types.tsx
@@ -0,0 +1,8 @@
+// Class for representing data to be shown in a driving lesson session view
+export type SessionViewData = 
+  { location     : string
+	, date         : Date
+	, instructors  : string[]
+	, participants : string[]
+  }
+
