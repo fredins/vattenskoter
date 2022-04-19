701e558daa56e0dbf1db4db5e1c752dca5beac75
falkenby <matildafalkenby@live.se>
2022-04-19 21:18:10 +0200

    Add colors and fix divs
    Added the calendar theme colors, played around a bit on where to have color and fixed some divs

--- web/src/app/components/Session.tsx
+++ web/src/app/components/Session.tsx
@@ -17,43 +17,41 @@ function listPeople(arr: string[]) {
 const Session : FC<SessionData> = data => {
 	return (
 		<div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-			<div className="flex items-top justify-center min-h-screen pt-20 px-0 pb-0 text-center sm:block sm:p-0">
+			<div className="flex justify-center min-h-screen pt-20 px-0 text-center sm:block sm:p-0">
 
 				<div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
 
 
-				<span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+				<span className="hidden sm:inline-block sm:align-bottom sm:h-screen" aria-hidden="true">&#8203;</span>
 
 
 				<div className="relative inline-block align-bottom bg-white rounded-t text-left overflow-hidden shadow-xl transform transition-all sm:mt-8 sm:align-middle sm:max-w-lg sm:w-full">
-					<div className="bg-white px-8 pt-5 pb-10 sm:p-6 sm:pb-4">
-						<div className="sm:flex sm:items-start">
-							<div className="mt-3 text-left sm:mt-0 sm:ml-4 sm:text-left">
-								<h2 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">Uppkörningstillfälle</h2>
-								<div className="mt-3">
-									<div className="mt-3">
-										<strong>Plats:</strong>
-										<p className="text-sm text-gray-500">{data.location}</p>
-									</div>
-									<div className="mt-3">
-										<strong>Tid:</strong>
-										<p className="text-sm text-gray-500">{data.from.toString()}</p>
-									</div>
-									<div className="mt-3">
-										<strong>Instruktörer:</strong>
-										<p className="text-sm text-gray-500">{listPeople(data.instructors)}</p>
-									</div>
-									<div className="mt-3">
-										<strong>Deltagare:</strong>
-										<p className="text-sm text-gray-500">{listPeople(data.participants)}</p>
-									</div>
-								</div>
+					<div className="relative w-full text-left">
+						<h1 className="mt-10 mb-5 px-8 w-full inline-flex justify-left text-3xl leading-6 font-bold text-black py-3" id="modal-title">Uppkörningstillfälle</h1>
+					</div>
+					<div className="px-8 sm:flex sm:items-start">
+						<div className="text-left sm:mt-0 sm:text-left">
+							<div className="mt-5">
+								<span className="font-medium text-theme-pink">Plats:</span>
+								<p className="text-sm text-gray-500">{data.location}</p>
+							</div>
+							<div className="mt-3">
+								<span className="font-medium text-theme-pink">Tid:</span>
+								<p className="text-sm text-gray-500">{data.from.toString()}</p>
+							</div>
+							<div className="mt-3">
+								<span className="font-medium text-theme-pink">Instruktörer:</span>
+								<p className="text-sm text-gray-500">{listPeople(data.instructors)}</p>
+							</div>
+							<div className="mt-3">
+								<span className="font-medium text-theme-pink">Deltagare:</span>
+								<p className="text-sm text-gray-500">{listPeople(data.participants)}</p>
 							</div>
 						</div>
 					</div>
-					<div className="bg-gray-50 px-8 py-3 sm:px-6 sm:flex sm:flex-row-reverse ">
-						<button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Redigera</button>
-						<button type="button" className="mt-3 mb-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Avbryt</button>
+					<div className="absolute bottom-0 bg-gray-50 px-8 py-3 sm:flex sm:flex-row-reverse w-full">
+						<button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-theme-pink text-base font-medium text-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-auto sm:ml-auto sm:w-auto sm:text-sm">Redigera</button>
+						<button type="button" className="mt-3 mb-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-auto sm:ml-auto sm:w-auto sm:text-sm">Avbryt</button>
 					</div>
 				</div>
 			</div>
@@ -64,8 +62,4 @@ const Session : FC<SessionData> = data => {
 
 export default Session;
 
-//<b>Plats:</b> {data.location}<br/>
-//<b>Tid:</b> {data.from.toString()}<br/>
-//<b>Instruktörer:</b><br/>
-//{listPeople(data.instructors)} <b>Deltagare:</b><br/>
-//{listPeople(data.participants)}
+
