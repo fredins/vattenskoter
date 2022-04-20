59e2ec374de8073607758e58a9ca1c8e9cbcd00e
Spektria <48885787+Spektria@users.noreply.github.com>
2022-04-20 12:37:22 +0200

    Add method to ping wix every 10 sec for testing purposes.

--- server/src/main/java/com/defLeppard/Application.java
+++ server/src/main/java/com/defLeppard/Application.java
@@ -1,8 +1,11 @@
 package com.defLeppard;
 
+import com.defLeppard.services.WixService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * Initializes the spring context and any other top level
@@ -16,6 +19,14 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	private WixService wixService;
+
+	@Scheduled(fixedRate = 1000 * 10) // ms, i.e. 10 sec
+	private void pingWix(){
+		System.out.println(wixService.call("hello"));
 	}
 
 }
