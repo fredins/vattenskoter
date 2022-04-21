e783b2e35d1ccce59985580ccc8db29f2ad012bc
Kablero <kaslju@student.chalmers.se>
2022-04-21 20:09:06 +0200

    Add instructor controller and test classes for the controllers

---
+++ server/src/main/java/com/defLeppard/controllers/InstructorController.java
@@ -0,0 +1,11 @@
+package com.defLeppard.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/instructors")
+public class InstructorController {
+}
