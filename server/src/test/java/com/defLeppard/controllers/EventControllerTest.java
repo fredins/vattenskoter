2147b39d9faa0e8c9c126175b1d9f4902b50f472
Tllija <badlands.hugo@gmail.com>
2022-04-22 20:14:03 +0200

    Add basic OK response test to EventsController.

--- server/src/test/java/com/defLeppard/controllers/EventControllerTest.java
+++ server/src/test/java/com/defLeppard/controllers/EventControllerTest.java
@@ -1,4 +1,32 @@
 package com.defLeppard.controllers;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalTime;
+import java.util.*;
+
+/**
+ *
+ * @author Hugo Ekstarand
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EventControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void getEventsOKTest(){
+        var resp = this.template.getForEntity("http://localhost:" + port + "/events", String.class);
+        Assertions.assertTrue(resp.hasBody() && resp.getStatusCode() == HttpStatus.OK);
+    }
 }
