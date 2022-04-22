4cf44658f2d5a7a8d82cc9912fb4c07c2e300220
Kablero <kaslju@student.chalmers.se>
2022-04-22 11:43:52 +0200

    Add general structure of InstructorController.

--- server/src/main/java/com/defLeppard/controllers/InstructorController.java
+++ server/src/main/java/com/defLeppard/controllers/InstructorController.java
@@ -4,8 +4,52 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 @CrossOrigin
 @RestController
 @RequestMapping("/instructors")
 public class InstructorController {
+
+    // Dummy list
+    // TODO: replace with database
+    private List<Map<String, String>> instructors =
+            List.of(
+                    Map.of(
+                            "name", "instructor1",
+                            "login", "123",
+                            "email", "instructor1@skoter.com"
+                    ),
+
+                    Map.of(
+                            "name", "instructor2",
+                            "login", "321",
+                            "email", "instructor2@skoter.com"
+                    ),
+
+                    Map.of(
+                            "name", "instructor3",
+                            "login", "admin",
+                            "email", "instructor3@skoter.com"
+                    )
+            );
+
+
+    @GetMapping("")
+    @ResponseBody
+    ResponseEntity<List<Map<String, String>>> getInstructors(){
+        return ResponseEntity.status(HttpStatus.OK).body(instructors);
+    }
+
+
+    // TODO: Login method
+    @GetMapping("/login")
+    @ResponseBody
+    ResponseEntity<?> logIn(){
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
 }
