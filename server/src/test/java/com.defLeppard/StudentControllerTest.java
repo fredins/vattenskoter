0dc747e836f59e1a4affb5cf2a938415f1ba80f4
Tllija <badlands.hugo@gmail.com>
2022-04-22 12:04:39 +0200

    Add get test for StudentController.

--- server/src/test/java/com.defLeppard/StudentControllerTest.java
+++ server/src/test/java/com.defLeppard/StudentControllerTest.java
@@ -1,4 +1,26 @@
 package com.defLeppard;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StudentControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void getStudentsOKTest(){
+        var resp = this.template.getForEntity("http://localhost:" + port + "/students", String.class);
+        Assertions.assertTrue(resp.hasBody() && resp.getStatusCode() == HttpStatus.OK);
+    }
+
 }
