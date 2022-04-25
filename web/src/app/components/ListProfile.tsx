a906af031ce63c03ad95172580d230f243b78522
fredins <fredin.martin@gmail.com>
2022-04-25 22:54:37 +0200

    Remove check icon

--- web/src/app/components/ListProfile.tsx
+++ web/src/app/components/ListProfile.tsx
@@ -19,7 +19,6 @@ const ListProfile: FC<Props> = ({ onChange, name, id }) => {
     <li className='flex justify-between mb-1 bg-red-100 border border-solid border-red-200 rounded'>
       <input className='bg-red-100 text-sm border border-transparent rounded outline-none  focus:border-solid focus:border-sky-300  focus:outline-sky-200 focus:outline-offset-0 transition-all pl-2 pr-2' value={name} onChange={onChange} />
       <div className='flex justify-between space-x-1'>
-        <BsCheck2 className='cursor-pointer' size='20px' />
         <BsX className='cursor-pointer' size='20px' />
         <IoMdPerson className='cursor-pointer' size='20px' />
       </div>
