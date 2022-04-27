8b6109f3666342bfec2a83cfe059a292f37a683d
Spektria <48885787+Spektria@users.noreply.github.com>
2022-04-27 22:07:33 +0200

    Add documentation

--- web/src/app/apis/StudentApi.tsx
+++ web/src/app/apis/StudentApi.tsx
@@ -1,7 +1,16 @@
 import { StudentData } from "../../types/types";
 import { ServerURL } from "./URIs";
 
+/**
+ * @author Hugo Ekstrand
+ * API for fetching student data.
+ */
 
+
+/**
+ * Fetches a list of all students
+ * @returns the promise list of all students
+ */
 export async function getStudents(): Promise<[StudentData]>{
     const res = await fetch(ServerURL + "/students");
     return res.json();
