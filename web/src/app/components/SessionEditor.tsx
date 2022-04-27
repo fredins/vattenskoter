7d6274a49f7d823f21d5032f00c84c063b925121
Spektria <48885787+Spektria@users.noreply.github.com>
2022-04-27 22:15:02 +0200

    Change hardcode instructor data to api data

--- web/src/app/components/SessionEditor.tsx
+++ web/src/app/components/SessionEditor.tsx
@@ -1,9 +1,10 @@
 import { useReducer, FC, useState } from 'react';
 import { Date_ } from 'react-awesome-calendar'
-import { SessionData, Either, StudentData } from '../../types/types'
+import { SessionData, Either, StudentData, InstructorData } from '../../types/types'
 import MultiInput from './MultiInput'
 import { FaLongArrowAltRight } from 'react-icons/fa'
 import { getStudents } from '../apis/StudentApi';
+import { getInstructors } from '../apis/InstructorApi';
 
 const SessionEditor: FC<Either<Date_, SessionData>> = ({ left, right }) => {
   if (right !== undefined)
@@ -29,6 +30,9 @@ const Form: FC<SessionData> = initState => {
 
   const [students, setStudents] = useState<[StudentData]>();
   getStudents().then(s => setStudents(s));
+
+  const [instructors, setInstructors] = useState<[InstructorData]>();
+  getInstructors().then(i => setInstructors(i));
 
   return (
     <div className='flex flex-col items-center bg-gray-500 h-screen'>
@@ -59,7 +63,7 @@ const Form: FC<SessionData> = initState => {
 
           <div className='mt-1 mb-3 border-b-2 pb-4'>
             <label className='text-lg' htmlFor="instructors">Instruktörer: </label>
-            <MultiInput name="instructors" options={['Erik', 'Sofia', 'Karl', 'Björn']} placeholder='Lägg till en instruktör' />
+            <MultiInput name="instructors" options={instructors == undefined ? [] : instructors.map(s => s.name)} placeholder='Lägg till en instruktör' />
           </div>
           <div className='mt-1 mb-1'>
             <label className='text-lg' htmlFor="students">Elever: </label>
