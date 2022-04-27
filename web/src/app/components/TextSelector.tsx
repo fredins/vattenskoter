d86aef08b864f1c119057e74409edf00b06aac70
Spektria <48885787+Spektria@users.noreply.github.com>
2022-04-27 21:21:33 +0200

    Add selected color

--- web/src/app/components/TextSelector.tsx
+++ web/src/app/components/TextSelector.tsx
@@ -1,35 +1,28 @@
-import React, { FC, useState } from 'react';
-
 /**
  * File contains TextSelector component responsible for searching and selecting approved inputs.
- * 
+ *
  * @author Hugo Ekstrand
  */
+
+import { FC, useState } from "react"
 
 /**
  * @field placeholder - Placeholder for input
  * @field selectables - Approved inputs
- * @field 
+ * @field
  */
-type TSelectorData = {
+ type TSelectorData = {
     placeholder?: string
     selectables: string[]
-    inputChangedCallback: (str: string) => void 
+    inputChangedCallback: (str: string) => void
 }
-
-
 const TextSelector: FC<TSelectorData> = ({placeholder, selectables, inputChangedCallback}) => {
-    
     // User text input value
     let [inputValue, inputSetter] = useState("");
-
     // All current predictions
     let [predictionsValue, predictionsSetter]  = useState(['']);
-
     // Index for the prediction. Note: -1 is for no prediction selected.
     let [predictionIndex, setPredictionIndex] = useState(-1);
-
-
     function onKeyDown(ev: React.KeyboardEvent<HTMLInputElement>){
         if(ev.key === "ArrowDown"){
             setPredictionIndex(Math.min(predictionIndex + 1, predictionsValue.length - 1));
@@ -40,29 +33,23 @@ const TextSelector: FC<TSelectorData> = ({placeholder, selectables, inputChanged
         if(ev.key === "Enter" && predictionIndex !== -1){
             onInputChange(predictionsValue[predictionIndex]);
         }
-
         console.log("Selected " + predictionIndex + ", " + predictionsValue[predictionIndex])
     }
-
     function onInputChange(str: string){
         inputSetter(str);
         predictionsSetter(str !== '' ? predictions(str, selectables) : []);
         setPredictionIndex(-1);
         inputChangedCallback(str);
     }
-
     return (
-        <div className='group relative'>
-            
-            <input className='input text-lg' autoComplete='true' placeholder={placeholder} value={inputValue} 
+        <div className='group relative dropdown  px-4 cursor-pointer font-bold tracking-wide'>
+            <input className='input text-lg' autoComplete='true' placeholder={placeholder} value={inputValue}
             onChange={ev => onInputChange(ev.target.value)} onKeyDown={onKeyDown}/>
-
-            <div className="group-hover:block dropdown-menu absolute hidden h-auto w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5" role="menu"
-                style={{zIndex: "99"}}>
+            <div className="group-hover:block dropdown-menu absolute hidden h-auto border-2 rounded z-20 bg-white" role="menu">
                 {
-                    predictionsValue.map((itm, i) => (
-                        <li className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-400" role="menuitem" id={'menu-item-' + i}
-                            key={i + '_' + itm} 
+                    predictionsValue.filter(s => s !== "").map((itm, i) => (
+                        <li className={"text-gray-700 block px-4 py-2 text-sm hover:bg-gray-400 border-b-2 rounded z-20" + (i == predictionIndex ? " bg-red-200 border-red-400" : "")} role="menuitem" 
+                            key={i + '_' + itm}
                             onClick={_ => onInputChange(itm)} >
                             {itm}
                         </li>
@@ -71,27 +58,24 @@ const TextSelector: FC<TSelectorData> = ({placeholder, selectables, inputChanged
             </div>
         </div>
     );
-} 
-
+}
 /**
  * Returns a list of predictions given a partial string input and a collection of allowed predictions.
  * @param partial - The partial string which will be predicted of.
- * @param collection - The allowed predictions 
+ * @param collection - The allowed predictions
  * @returns the subset of allowed predictions which are predictions to the given partial string.
  */
 function predictions(partial: string, collection: string[]): string[] {
-    
     // TODO: Cleanup or change prediction
     // Currently it matches on every word in the string.
     // E.g. partial match of the second word in a string in collection
     // Or the second word in partial could match a string in collection
     return collection
         .filter(str => str.toLowerCase() !== partial.toLowerCase())
-        .filter(str => 
+        .filter(str =>
             str.toLowerCase()
                 .split(' ') // We want to match any word. E.g. surnames and first name
-                .filter(sp => 
+                .filter(sp =>
                     partial.toLowerCase().split(' ').filter(p => sp.startsWith(p)).length > 0).length > 0);
 }
-
 export default TextSelector
