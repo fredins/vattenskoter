7814ee947b62d2b68c65ff5f48fadcb4fb238a94
fredins <fredin.martin@gmail.com>
2022-04-27 09:05:22 +0200

    Populate Calendar with events from db

--- web/src/app/components/Calendar.tsx
+++ web/src/app/components/Calendar.tsx
@@ -1,10 +1,18 @@
 import { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
 import AwesomeCalendar, { Event_ } from 'react-awesome-calendar';
+import { useQuery } from 'react-query'
+import { getEvents } from '../apis/EventApi'
+import { SessionData } from '../../types/types'
+import { map } from 'ramda'
 
-const Calendar: FC<{events : Event_[]}> = ({ events }) => {
+const Calendar: FC = () => {
   const navigate = useNavigate()
-  
+  const query = useQuery('events', getEvents)
+  const events = map(
+    toEvent 
+    , query.data === undefined ? [] : query.data)
+
   return (
    <div className='pl-4 pr-4'>
     <AwesomeCalendar 
@@ -14,6 +22,19 @@ const Calendar: FC<{events : Event_[]}> = ({ events }) => {
     />
    </div>
   );
+}
+
+
+function toEvent(session: SessionData): Event_ {
+  return (
+    {
+      id: session.id,
+      color: '#fd3153',
+      from: session.from,
+      to: session.to,
+      title: session.title,
+    }
+  )
 }
 
 export default Calendar
