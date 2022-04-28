f56c461c80164d6ef52be20592fdfc19a2adb208
fredins <fredin.martin@gmail.com>
2022-04-28 20:01:59 +0200

    Use request params

--- web/src/app/apis/EventApi.tsx
+++ web/src/app/apis/EventApi.tsx
@@ -1,7 +1,8 @@
 import { SessionData } from '../../types/types'
 
-export async function getEvents(): Promise<SessionData[]>{
-  const res = await fetch("http://localhost:8080/events")
+export async function getEvents(year : number): Promise<SessionData[]>{
+  const res = await fetch(
+    `http://localhost:8080/events?from=${new Date(year, 1, 1).toISOString()}&to=${new Date(year, 11, 31).toISOString()}`)
   return res.json()
 }
 
