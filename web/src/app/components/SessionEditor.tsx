82e3b7404cbe5b4766785390035790ede732d8bf
fredins <fredin.martin@gmail.com>
2022-04-29 19:16:41 +0200

    Add 'back' functionality to modal buttons

--- web/src/app/components/SessionEditor.tsx
+++ web/src/app/components/SessionEditor.tsx
@@ -48,6 +48,7 @@ const Form: FC<SessionData> = (initState) => {
   // Generalize extraction of names
   interface HasName { name: string }
   const getNames = (list: HasName[] | undefined) => orElse(() => list?.map(s => s.name), [])(null);
+  const navigate = useNavigate()
 
   return (
     <div className='fixed inset-0 z-10 scroll overflow-y-hidden'>
