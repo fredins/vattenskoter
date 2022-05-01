4550cd0f630e82a6bc2561b87dbafcc0e8100c8e
Willeeez <Schmitz.william@hotmail.com>
2022-05-01 11:26:13 +0200

    Add test data for database

--- server/src/main/java/com/defLeppard/services/Student.java
+++ server/src/main/java/com/defLeppard/services/Student.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -22,11 +23,13 @@ private String studentName;
      * @return Returns the read JSON objects into java objects
      *
      */
+
+
     public static List<Student> createStudents(String json) throws IOException {
 
         ObjectMapper mapper = new ObjectMapper();
 
-        List<Student> students = mapper.readValue(json, new TypeReference<List<Student>>(){});
+        List<Student> students = mapper.readValue(new File(json), new TypeReference<List<Student>>(){});
 
         return students;
     }
---
+++ server/src/main/resources/database.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Location( 
+	name TEXT PRIMARY KEY NOT NULL); 
+
+CREATE TABLE Session( 
+	idnr INT NOT NULL,
+	title TEXT NOT NULL,
+	fromdate TIMESTAMP NOT NULL,
+	todate TIMESTAMP NOT NULL,
+	location TEXT REFERENCES Location(name) NOT NULL,
+	PRIMARY KEY (idnr, location)); 
+
+CREATE TABLE Instructor( 
+	login TEXT PRIMARY KEY NOT NULL, 
+	email TEXT NOT NULL, 
+	name TEXT NOT NULL); 
+
+CREATE TABLE Student(  
+	email TEXT PRIMARY KEY NOT NULL,
+	name TEXT NOT NULL);
+	 
+
+CREATE TABLE EducationalMoment(
+	name TEXT PRIMARY KEY NOT NULL,
+	description TEXT NOT NULL); 
+	
+CREATE TABLE StudentEducationalMoments(
+	educationalMoment TEXT REFERENCES EducationalMoment(name) PRIMARY KEY NOT NULL,
+	studentEmail TEXT REFERENCES Student(email),
+	completed BOOLEAN NOT NULL); 
+
+CREATE TABLE Attend( 
+	studentEmail TEXT REFERENCES Student(email) NOT NULL,
+	instructor TEXT REFERENCES Instructor(login) NOT NULL,
+	sessionIdnr INT NOT NULL,
+	sessionLocation TEXT NOT NULL,
+	FOREIGN KEY(sessionIdnr, sessionLocation) REFERENCES Session(idnr,location)); 
+	
+
