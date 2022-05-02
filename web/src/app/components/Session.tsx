9bfa154c785d2f2afe2a28b977844d06420600fe
funcorange <love.njal@telia.com>
2022-05-02 16:52:42 +0200

    Make edit button link to edit page

--- web/src/app/components/Session.tsx
+++ web/src/app/components/Session.tsx
@@ -50,7 +50,9 @@ const Session : FC<SessionData> = data => {
 					</div>
 				</div>
 				<div className="absolute bottom-0 w-full sm:relative bg-gray-50 px-4 py-6 sm:px-6 sm:flex sm:flex-row-reverse">
-					<button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-theme-pink text-base font-medium text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-theme-pink sm:ml-3 sm:w-auto sm:text-sm">Redigera</button>
+					<a href={"/session/" + data.id + "/edit"}>
+						<button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-theme-pink text-base font-medium text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-theme-pink sm:ml-3 sm:w-auto sm:text-sm">Redigera</button>
+					</a>
 					<button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Avbryt</button>
 				</div>
 			</div>
