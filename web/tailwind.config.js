a46fde8c5ecbe5eb540a7ba780839489405011da
falkenby <matildafalkenby@live.se>
2022-05-05 18:08:47 +0200

    Add colors
    Added the colors which were agreed upon from the stakeholder through PO.

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
     extend: {
       colors: {
         'theme-pink' : '#fd3153',
+        'background-dark' : '#1C1C1E',
+        'text-main' : '#F2F2F7',
+        'text-sec' : '#EBEBF5',
+        'box-background' : '#767680',
       }
     },
   },
