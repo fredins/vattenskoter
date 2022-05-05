74f9201a7fc1fca601894d3a131b81af85f3030f
falkenby <matildafalkenby@live.se>
2022-05-05 19:03:38 +0200

    Remove pink color
    Removed the pink theme color

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'theme-pink' : '#fd3153',
         'background-dark' : '#1C1C1E',
         'text-main' : '#F2F2F7',
         'text-sec' : '#EBEBF5',
