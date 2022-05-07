0feb409759fdece8cf9109966fe4912a7728fe9b
RenatoRR <73895526+RenatoRR@users.noreply.github.com>
2022-05-07 21:57:33 +0200

    Insert students mockdata

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -14,4 +14,18 @@ INSERT INTO Session VALUES (1,'Förarbevis', TO_TIMESTAMP('2022-05-03 12:00:00',
                             (5,'Förarbevis Teoriutbildning', TO_TIMESTAMP('2022-05-07 9:00:00', 'YYYY-MM-DD HH24-MI'), TO_TIMESTAMP('2022-05-07 10:00:00', 'YYYY-MM-DD HH24-MI:SS'), 'Göteborg')
                      ON CONFLICT DO NOTHING;
 
+--INSERT INTO Instructor VALUES
+INSERT INTO Student VALUES ('renny99@outlook.com', 'Renato Radevski'),
+                            ('jonte32@gmail.com', 'Jonathan Petersson'),
+                            ('jessimail@hotmail.com', 'Jessica Andersson'),
+                            ('addi@live.se', 'Adin Wilson'),
+                            ('tovnils@gmail.com', 'Tove Nilsson'),
+                            ('andres@hotmail.se', 'Andreas Johansson')
+                     ON CONFLICT DO NOTHING;
+
+--INSERT INTO EducationalMoment VALUES
+--INSERT INTO StudentEducationalMoments VALUES
+--INSERT INTO Attend VALUES
+
+SELECT * FROM Session;
 
