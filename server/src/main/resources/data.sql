159b5b0b87774e5d7f18695007c5a9f4ac4a6106
Spektria <48885787+Spektria@users.noreply.github.com>
2022-05-10 13:04:56 +0200

    Add test mockdata to db

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -8,7 +8,7 @@ INSERT INTO Location VALUES ('Vänersborg'), ('Göteborg')
                      ON CONFLICT DO NOTHING;
 
 INSERT INTO Session VALUES (1,'Förarbevis', TO_TIMESTAMP('2022-05-03 12:00:00', 'YYYY-MM-DD HH24-MI'), TO_TIMESTAMP('2022-05-03 13:00:00', 'YYYY-MM-DD HH24-MI:SS'), 'Kungsbacka'),
-                            (2,'Förarbevis', TO_TIMESTAMP('2022-05-04 12:00:00', 'YYYY-MM-DD HH24-MI'), TO_TIMESTAMP('2022-05-04 13:00:00', 'YYYY-MM-DD HH24-MI:SS'), 'Vänersborg'),
+                            (2,'periodTest', TO_TIMESTAMP('2022-01-01 00:00:00', 'YYYY-MM-DD HH24-MI'), TO_TIMESTAMP('2022-01-01 10:00:00', 'YYYY-MM-DD HH24-MI:SS'), 'Vänersborg'),
                             (3,'Förarbevis Teoriutbildning', TO_TIMESTAMP('2022-05-05 9:00:00', 'YYYY-MM-DD HH24-MI'), TO_TIMESTAMP('2022-05-05 10:00:00', 'YYYY-MM-DD HH24-MI:SS'), 'Göteborg'),
                             (4,'Förarbevis', TO_TIMESTAMP('2022-05-06 12:00:00', 'YYYY-MM-DD HH24-MI'), TO_TIMESTAMP('2022-05-06 13:00:00', 'YYYY-MM-DD HH24-MI:SS'), 'Vänersborg'),
                             (5,'Förarbevis Teoriutbildning', TO_TIMESTAMP('2022-05-07 9:00:00', 'YYYY-MM-DD HH24-MI'), TO_TIMESTAMP('2022-05-07 10:00:00', 'YYYY-MM-DD HH24-MI:SS'), 'Göteborg')
