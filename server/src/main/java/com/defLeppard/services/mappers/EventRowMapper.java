54563fa06877b400de1118e62d8ec8092014fd83
Spektria <48885787+Spektria@users.noreply.github.com>
2022-05-16 13:10:42 +0200

    Change from getDate to getTimestamp

--- server/src/main/java/com/defLeppard/services/mappers/EventRowMapper.java
+++ server/src/main/java/com/defLeppard/services/mappers/EventRowMapper.java
@@ -15,7 +15,7 @@ class EventRowMapper implements RowMapper<Event> {
     @Override
     public Event mapRow(ResultSet rs, int rowNum) throws SQLException {
         return new Event(rs.getInt("idnr"), rs.getString("title")
-                        , rs.getDate("fromDate"), rs.getDate("toDate")
+                        , rs.getTimestamp("fromDate"), rs.getTimestamp("toDate")
                         , new String[]{}        // Note. A bit wonky because of limits in sql query instructors and participants cannot be found in same query.
                         , new Student[]{}
                         , rs.getString("location"));
