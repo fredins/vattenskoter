efe54b39228ba653f96ddb53c8cb564a7f257d95
fredins <fredin.martin@gmail.com>
2022-05-17 09:07:57 +0200

    TextSelector: Add scroll on overflow

--- web/src/app/components/TextSelector.tsx
+++ web/src/app/components/TextSelector.tsx
@@ -54,7 +54,12 @@ function TextSelector({ placeholder, selectables, onSubmit}: Props, ref: Forward
         ref={ref}
       />
 
-      <div className="peer-focus:block group-hover:block dropdown-menu absolute hidden h-auto border-l-2 border-r-2 rounded z-20 bg-white" role="menu">
+      <div 
+        className="peer-focus:block group-hover:block dropdown-menu absolute 
+                   hidden h-auto border-l-2 border-r-2 rounded z-20 bg-white
+                   overflow-y-auto max-h-52" 
+        role="menu"
+      >
         {
           pred.filter(s => s !== "").map((itm, i) => (
             <li className={`text-gray-700 block px-4 py-2 text-sm hover:bg-gray-400 border-b-2 rounded z-20 ${i === predIndex ? ' bg-red-200 border-red-400' : ''}`} role="menuitem"
