53d7a3d8c1161b7c362f9cbd8e1a1022270cfb0e
Tllija <badlands.hugo@gmail.com>
2022-05-18 15:01:36 +0200

    Add hardcoded remove date offset

--- server/src/main/java/com/defLeppard/entities/Event.java
+++ server/src/main/java/com/defLeppard/entities/Event.java
@@ -1,8 +1,11 @@
 package com.defLeppard.entities;
 
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Objects;
+import java.util.logging.SimpleFormatter;
 
 /**
  * Record for events. Though it does differentiate from sql database with having
@@ -22,6 +25,20 @@ public record Event(int id, String title, Date from, Date to, String[] instructo
             if(toString().isBlank())
                 throw new RuntimeException("Blank field in Event!");
         });
+
+
     }
 
+    /**
+     * Returns this event, but without timezone offsets. I.E. sets the timezone of the dates to and from to UTC.
+     * @return the event without timezone offset
+     */
+    public Event asUTC(){
+        final long HOUR = 3600*1000;
+        Date from = new Date(this.from.getTime() + 2 * HOUR);
+        Date to = new Date(this.to.getTime() +  + 2 * HOUR);
+        if(id == 1)
+            System.out.println("From :: " + from + "  -> " + to);
+        return new Event(this.id,this.title,from,to,this.instructors,this.participants,this.location);
+    }
 }
