8b93ce9a9cdb36270a7cb3e6c2ace75e5dd12422
RenatoRR <73895526+RenatoRR@users.noreply.github.com>
2022-05-19 18:52:38 +0200

    Fix one line

--- web/src/app/components/StudentProfile.tsx
+++ web/src/app/components/StudentProfile.tsx
@@ -41,7 +41,7 @@ const StudentProfile: FC<Student> = data => {
         <div
           className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
           aria-hidden="true"
-          onClick={() => navigate(-1)}
+          onClick={() => navigate('/')}
         />
 
         <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
