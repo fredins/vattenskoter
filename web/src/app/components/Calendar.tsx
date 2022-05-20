99d7d66d6241310346637e34a632e40e3cf82aee
fredins <fredin.martin@gmail.com>
2022-05-20 17:21:31 +0200

    Make calendar use timezone compensated dates

--- web/src/app/components/Calendar.tsx
+++ web/src/app/components/Calendar.tsx
@@ -6,9 +6,10 @@ import { map } from 'ramda'
 import { useSwipeable } from 'react-swipeable'
 
 
-type Props = 
-  { onChange : (s: CalendarState) => void 
-    sessions : SessionData[]
+type Props =
+  {
+    onChange: (s: CalendarState) => void
+    sessions: SessionData[]
   }
 
 /** 
@@ -18,7 +19,7 @@ type Props =
  * @param props.onChange - Change handler for CalendarState
  * @param props.sessions 
  */
-function Calendar({ onChange, sessions } : Props) {
+function Calendar({ onChange, sessions }: Props) {
   const location = useLocation()
   const navigate = useNavigate()
 
@@ -34,7 +35,7 @@ function Calendar({ onChange, sessions } : Props) {
    * 
    * @see {@link https://reactjs.org/docs/hooks-reference.html#useref}
    * @see {@link https://github.com/fredins/react-awesome-calendar}
-   */ 
+   */
   const ref = useRef<AwesomeCalendarComponent>(null)
 
   /**
@@ -57,7 +58,16 @@ function Calendar({ onChange, sessions } : Props) {
         events={map(toEvent, sessions)}
         onChange={onChange}
         onClickEvent={id => navigate(`session/${id}`, { state: { background: location } })}
-        onClickTimeLine={date => navigate('/newsession', { state: { background: location, date: date } })}
+        onClickTimeLine={date => navigate('/newsession', {
+          state: {
+            background: location,
+            date: {
+              ...date,
+              /* May get negative hours depending on time and time zone */
+              hour: date.hour + new Date().getTimezoneOffset() / 60 
+            }
+          }
+        })}
         ref={ref as unknown as LegacyRef<Component<CalendarProps, any, any>> | undefined}
       />
     </div>
@@ -67,19 +77,39 @@ function Calendar({ onChange, sessions } : Props) {
 
 /** 
 * Function for mapping SessionData to CalendarEvent 
+* 
 * @param session
+* 
 * @returns a calendar event
+* 
+* @remarks The calendar doesn't handle time zones at all.
 */
 function toEvent(session: SessionData): CalendarEvent {
   return (
     {
       id: session.id,
       color: '#fd3153',
-      from: session.from,
-      to: session.to,
+      from: addTimeOffset(new Date(session.from)),
+      to: addTimeOffset(new Date(session.to)),
       title: session.title,
     }
   )
 }
+
+/**
+ * Add locale time offset to Date
+ * 
+ * @param date 
+ * 
+ * @returns Date adjusted for timezone.
+ * 
+ * @remarks 
+ * getTimezoneOffset returns negative values for time zones ahead of UTC.
+ * This function is useful for components that don't handle timezones
+ */
+function addTimeOffset(date: Date): Date {
+  return new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+}
+
 
 export default Calendar
